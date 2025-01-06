d14c75b679e441b2b1bc6053f6857156e7ad5f50
tzniceguy <joeshimbi@gmail.com>
2025-01-06 12:32:57 +0300

    mockup for related products

--- lib/data.ts
+++ lib/data.ts
@@ -6,3 +6,12 @@ export const products = [
     {id:5, title:"UZI", img_url:"/products/uzi-pro-5404-600x272.jpg", description:"",price:1000, stock:10},
     {id:6, title:"MASADA", img_url:"/products/masada-1983x533-1024x275-1-600x161.jpg", description:"",price:1000, stock:10},
 ]
+
+export const relatedProducts = [
+    {id:1, title:"ARAD", img_url:"/products/arad7studioDMR-600x211.jpg", description:"", price:1000, stock:10},
+    {id:2, title:"DAN", img_url:"/products/dan-9004-600x272.jpg", description:"", price:1000, stock:10},
+    {id:3, title:"JERICHO", img_url:"/products/JERICHO_F_IWI_8592-600x272.jpg", description:"", price:1000, stock:10},
+    {id:4, title:"TAVOR", img_url:"/products/tavor-tar-flattop-4059-1-600x272.jpg", description:"",price:1000, stock:10},
+    {id:5, title:"UZI", img_url:"/products/uzi-pro-5404-600x272.jpg", description:"",price:1000, stock:10},
+    {id:6, title:"MASADA", img_url:"/products/masada-1983x533-1024x275-1-600x161.jpg", description:"",price:1000, stock:10},
+]
