86b789be146e8720de27fe5c010e27d839e63f3d
tzniceguy <joeshimbi@gmail.com>
2025-01-10 15:33:40 +0300

    logout function

--- api/api.ts
+++ api/api.ts
@@ -1,12 +1,17 @@
 import axiosInstance from "./axios-instance";
 
-export interface registerData {
+export interface RegisterData {
   username: string;
   password: string;
   firstname: string;
   email: string;
   lastname: string;
   phonenumber: string;
+}
+
+export interface LoginData {
+  username: string;
+  password: string;
 }
 
 interface RegisterResponse {
@@ -20,13 +25,23 @@ interface RegisterResponse {
 }
 
 export const register = async (
-  formData: registerData,
+  formData: RegisterData,
 ): Promise<RegisterResponse> => {
   try {
     const response = await axiosInstance.post("register/", formData);
     return response.data;
   } catch (error) {
     console.error("registering failed:", error);
+    throw error;
+  }
+};
+
+export const login = async (formData: LoginData) => {
+  try {
+    const response = await axiosInstance.post("login/", formData);
+    return response.data;
+  } catch (error) {
+    console.error("login failed:", error);
     throw error;
   }
 };
