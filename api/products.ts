aeb865ae276aa54e77c81a52a82b5c9910107b74
tzniceguy <joeshimbi@gmail.com>
2025-01-13 20:30:10 +0300

    dyanically render product detail
    a function that calls product details endpoint dynamically using slug

--- api/products.ts
+++ api/products.ts
@@ -23,3 +23,12 @@ export const getProducts = async (): Promise<Product[]> => {
     throw error;
   }
 };
+export const getProductDetail = async (slug: string): Promise<Product> => {
+  try {
+    const response = await axiosInstance.get(`products/${slug}/`);
+    return response.data;
+  } catch (error) {
+    console.error("fetching product detail failed:", error);
+    throw error;
+  }
+};
