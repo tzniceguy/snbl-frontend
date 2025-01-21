92a6f5ffbbf29356266b97c52dce21e967ecdc52
tzniceguy <joeshimbi@gmail.com>
2025-01-21 21:08:24 +0300

    handling auth token on logout

--- api/api.ts
+++ api/api.ts
@@ -48,11 +48,28 @@ export const login = async (formData: LoginData) => {
 
 export const logout = async () => {
   try {
-    const response = await axiosInstance.post("logout/");
-    return response.data;
+    const authToken = localStorage.getItem("authToken");
+    //only attempt to logout if there is authtoken
+    if (authToken) {
+      //ensuring auth token in included in the request header
+      axiosInstance.defaults.headers.common["Authorization"] =
+        `Bearer ${authToken}`;
+
+      const response = await axiosInstance.post("logout/");
+
+      //clear auth headers after logout
+      delete axiosInstance.defaults.headers.common["Authorization"];
+
+      return response.data;
+    }
   } catch (error) {
     console.error("logout failed:", error);
     throw error;
+  } finally {
+    //clear auth token from local storage
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("userProfile");
   }
 };
 
