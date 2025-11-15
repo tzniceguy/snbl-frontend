6fed67a0b9387b8b9478c8755d60bc277f31a59b
Joe <52755372+tzniceguy@users.noreply.github.com>
2025-11-15 09:02:53 +0300

    Update base URL in axios instance

--- api/axios-instance.ts
+++ api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://snbl-api.deploy.tz/api/",
+  baseURL: "https://snbl.deploy.tz/api/",
   headers: {
     "Content-Type": "application/json",
   },
