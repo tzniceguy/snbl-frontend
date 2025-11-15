791f53c06eeebacf26ce21445d17df38d79d1fae
Joe Shimbi <joeshimbi@gmail.com>
2025-11-15 10:23:18 +0300

    chore:migrate the lockfile to new bun text lockfile

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**", // Allows any hostname
-        pathname: "/media/product-images/**",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "snbl.deploy.tz",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
